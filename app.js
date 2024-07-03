bb76642f48ef6479e6ea1e8841ad45c0e7c08b51
amine hamdi <mohamedamine.hamdi@insat.ucar.tn>
2024-07-03 14:16:37 +0100

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -16,6 +23,15 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.set('trust proxy', 1);
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100
+}));
 // extra packages
 
 // routes
